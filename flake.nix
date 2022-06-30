edc489a49e90f606e6ff5b09cd0fa0e620a4e986
Astavie <astavie@pm.me>
2022-06-30 21:25:35 +0200

    flex command, better zsh prompt

--- flake.nix
+++ flake.nix
@@ -10,51 +10,57 @@
       username = "astavie";
 
       stateVersion = "22.05";
-      configurationRevision = if self ? rev then self.rev else null;
 
-      mkSystem = { modules, system }:
-        let
-        in nixpkgs.lib.nixosSystem {
-          inherit system;
-          specialArgs = {
-            inherit username;
+      mkHome = { name, system, modules }:
+      {
+        homeConfigurations = {
+          "${name}" = home-manager.lib.homeManagerConfiguration {
+            username = name;
+            homeDirectory = "/home/${name}";
+            inherit system;
             inherit stateVersion;
-            inherit configurationRevision;
+            configuration = { ... }: { imports = modules; };
           };
-          modules =
-            modules.hardware ++
-            modules.system ++
-            [home-manager.nixosModules.home-manager{
-                home-manager.useGlobalPkgs = true;
-                home-manager.useUserPackages = true;
-                home-manager.users.${username} = { modules, ... }: { imports = modules; };
-                home-manager.extraSpecialArgs.modules = modules.home;
-            }];
         };
-
-    in {
-
-      nixosConfigurations = {
-
-        # VirtualBox demo of NixOS
-        nixos = mkSystem {
-          system = "x86_64-linux";
-
-          modules.hardware = [
-            ./hardware/vb_demo.nix
-          ];
-          modules.system = [
-            ./system/default.nix
-            ./system/vb.nix
-          ];
-          modules.home = [
-            ./home/default.nix
-            ./home/vb.nix
-          ];
-        };
-
       };
 
-    };
+      mkSystem = { name, system, modules, args ? {} }:
+      {
+        nixosConfigurations = {
+          "${name}" = nixpkgs.lib.nixosSystem {
+            inherit system;
+            inherit modules;
+            specialArgs = {
+              inherit username;
+              inherit stateVersion;
+              inherit self;
+              flakeDir = null;
+            } // args;
+          };
+        };
+      };
+
+    in
+      mkHome {
+        name = username;
+        system = "x86_64-linux";
+
+        modules = [
+          ./home/default.nix
+          ./home/vb.nix
+        ];
+      } //
+      mkSystem {
+        name = "nixos";
+        system = "x86_64-linux";
+
+        args.flakeDir = "/media/sf_dotfiles";
+
+        modules = [
+          ./hardware/vb_demo.nix
+          ./system/default.nix
+          ./system/vb.nix
+        ];
+      };
 
 }
