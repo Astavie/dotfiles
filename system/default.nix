9e1c94024057c07592fb171bb457b625babe31f9
Astavie <astavie@pm.me>
2022-07-22 20:59:54 +0200

    remove update home-manager inside systemd

--- system/default.nix
+++ system/default.nix
@@ -11,6 +11,9 @@ let
     if [ "$EUID" -eq 0 ]
     then
       ${pkgs.nixos-rebuild}/bin/nixos-rebuild switch --flake ''${1:-${dir}}
+      ${lib.concatStrings (lib.mapAttrsToList (username: usercfg: ''
+        sudo -u ${username} ${pkgs.home-manager}/bin/home-manager switch --flake ''${1:-${dir}}
+      '') users)}
     else
       ${pkgs.home-manager}/bin/home-manager switch --flake ''${1:-${dir}}
     fi
@@ -68,63 +71,4 @@ in
     extraGroups = lib.mkIf (usercfg ? superuser && usercfg.superuser) [ "wheel" "networkmanager" ];
   }) users;
 
-  systemd.services = lib.mapAttrs' (username: usercfg:
-    lib.nameValuePair "home-manager-${utils.escapeSystemdPath username}" {
-      # Copied from https://github.com/nix-community/home-manager/blob/master/nixos/default.nix
-      description = "Home Manager environment for ${username}";
-      wantedBy = [ "multi-user.target" ];
-      wants = [ "nix-daemon.socket" ];
-      after = [ "nix-daemon.socket" ];
-      before = [ "systemd-user-sessions.service" ];
-
-      environment = {};
-
-      unitConfig = { RequiresMountsFor = if usercfg ? home then usercfg.home else "/home/${username}"; };
-
-      stopIfChanged = false;
-
-      serviceConfig = {
-        User = username;
-        Type = "oneshot";
-        RemainAfterExit = "yes";
-        TimeoutStartSec = 90;
-        SyslogIdentifier = "hm-activate-${username}";
-
-        ExecStart = let
-          systemctl =
-            "XDG_RUNTIME_DIR=\${XDG_RUNTIME_DIR:-/run/user/$UID} systemctl";
-
-          sed = "${pkgs.gnused}/bin/sed";
-
-          exportedSystemdVariables = lib.concatStringsSep "|" [
-            "DBUS_SESSION_BUS_ADDRESS"
-            "DISPLAY"
-            "WAYLAND_DISPLAY"
-            "XAUTHORITY"
-            "XDG_RUNTIME_DIR"
-          ];
-
-          setupEnv = pkgs.writeScript "hm-setup-env" ''
-            #! ${pkgs.runtimeShell} -el
-            # The activation script is run by a login shell to make sure
-            # that the user is given a sane environment.
-            # If the user is logged in, import variables from their current
-            # session environment.
-            eval "$(
-              ${systemctl} --user show-environment 2> /dev/null \
-              | ${sed} -En '/^(${exportedSystemdVariables})=/s/^/export /p'
-            )"
-            exec "$1"
-          '';
-        in "${setupEnv} ${flex}/bin/flex";
-      };
-    }
-  ) users;
-
-  nix.settings.trusted-users =
-    lib.attrNames (
-      lib.filterAttrs
-        (_: usercfg: usercfg ? superuser && usercfg.superuser)
-        users
-    );
 }
