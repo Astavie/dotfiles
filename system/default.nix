b6bec6be3f4ac735f3090adcd4bf309844d997e1
Astavie <astavie@pm.me>
2022-07-22 21:11:41 +0200

    also write access i guess

--- system/default.nix
+++ system/default.nix
@@ -24,7 +24,7 @@ let
     then
       DIR=$(mktemp -d)
       trap 'rm -rf -- "$DIR"' EXIT
-      chmod o=r $DIR
+      chmod o=rw $DIR
       ${pkgs.nix}/bin/nix flake clone ${inputs.flakeRepo} --dest $DIR
     fi
     ${flexInner "$DIR"}
