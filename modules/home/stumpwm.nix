c0e143c86b83d8391134724e3fe193e8c2747421
Astavie <astavie@pm.me>
2022-09-05 21:10:12 +0200

    stumpwm gaps

--- modules/home/stumpwm.nix
+++ modules/home/stumpwm.nix
@@ -7,6 +7,12 @@
   ];
 
   home.file.".stumpwmrc".source = ../../config/.stumpwmrc;
+  home.file.".stumpwm.d/modules".source = pkgs.fetchFromGitHub {
+    owner = "stumpwm";
+    repo = "stumpwm-contrib";
+    rev = "6d4584f01dec0143a169186df1608860d1aa1ef0";
+    sha256 = "sha256-ts+MPFtLCjj6T2MuOxWSKfNCv9gvS348SV6IztUfx8M=";
+  };
 
   home.file.".config/sx/sxrc" = {
     executable = true;
