5789902c87c7a00b2874dc3e4d52c7a19f7ce485
Astavie <astavie@pm.me>
2023-02-21 13:33:36 +0100

    cursor

--- modules/system/steam.nix
+++ modules/system/steam.nix
@@ -3,6 +3,7 @@
 {
   programs.steam.enable = true;
   programs.steam.remotePlay.openFirewall = true;
+  hardware.steam-hardware.enable = true;
   hardware.opengl.enable = true;
   hardware.opengl.driSupport = true;
   hardware.opengl.driSupport32Bit = true;
