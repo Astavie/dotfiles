edb9529beed9eacc2af3dce694926fc0463bf964
Astavie <astavie@pm.me>
2024-04-22 19:35:39 +0200

    chicago

--- system/terrestrial.nix
+++ system/terrestrial.nix
@@ -33,10 +33,9 @@
       ];
 
       ssh.enable = true;
-      hyprland.enable = true;
 
       modules = [
-        ../home/desktop-glassmorphism.nix
+        ../home/desktop-chicago95.nix
         ../home/discord.nix
         ../home/firefox.nix
         ../home/git.nix
@@ -54,7 +53,10 @@
         }
       ];
 
-      backup.directories = ["obsidian/.config/obsidian"];
+      backup.directories = [
+        "obsidian/.config/obsidian"
+        "xfce/.config/xfce4"
+      ];
     };
     streamer = {
       packages = with pkgs; [
@@ -127,14 +129,48 @@
 
     # nvidia with modes for dual monitors
 
-    # NVIDIA has flickering issues on xwayland since 545, so we downgrade to 535
-    hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.production;
-    hardware.nvidia.modesetting.enable = true;
-    services.xserver.videoDrivers = [ "nvidia" ];
+    hardware.nvidia = {
+      modesetting.enable = true;
+      nvidiaSettings = true;
 
-    # services.xserver.screenSection = ''
-    #   Option "metamodes" "nvidia-auto-select { ForceCompositionPipeline = On, ForceFullCompositionPipeline=On, AllowGSYNCCompatible=On }, nvidia-auto-select { ForceCompositionPipeline = On, ForceFullCompositionPipeline=On, AllowGSYNCCompatible=On }"
-    # '';
+      package = let 
+        rcu_patch = pkgs.fetchpatch {
+          url = "https://github.com/gentoo/gentoo/raw/c64caf53/x11-drivers/nvidia-drivers/files/nvidia-drivers-470.223.02-gpl-pfn_valid.patch";
+          hash = "sha256-eZiQQp2S/asE7MfGvfe6dA/kdCvek9SYa/FFGp24dVg=";
+        };
+      in config.boot.kernelPackages.nvidiaPackages.mkDriver {
+          version = "535.154.05";
+          sha256_64bit = "sha256-fpUGXKprgt6SYRDxSCemGXLrEsIA6GOinp+0eGbqqJg=";
+          sha256_aarch64 = "sha256-G0/GiObf/BZMkzzET8HQjdIcvCSqB1uhsinro2HLK9k=";
+          openSha256 = "sha256-wvRdHguGLxS0mR06P5Qi++pDJBCF8pJ8hr4T8O6TJIo=";
+          settingsSha256 = "sha256-9wqoDEWY4I7weWW05F4igj1Gj9wjHsREFMztfEmqm10=";
+          persistencedSha256 = "sha256-d0Q3Lk80JqkS1B54Mahu2yY/WocOqFFbZVBh+ToGhaE=";
+
+          #version = "550.40.07";
+          #sha256_64bit = "sha256-KYk2xye37v7ZW7h+uNJM/u8fNf7KyGTZjiaU03dJpK0=";
+          #sha256_aarch64 = "sha256-AV7KgRXYaQGBFl7zuRcfnTGr8rS5n13nGUIe3mJTXb4=";
+          #openSha256 = "sha256-mRUTEWVsbjq+psVe+kAT6MjyZuLkG2yRDxCMvDJRL1I=";
+          #settingsSha256 = "sha256-c30AQa4g4a1EHmaEu1yc05oqY01y+IusbBuq+P6rMCs=";
+          #persistencedSha256 = "sha256-11tLSY8uUIl4X/roNnxf5yS2PQvHvoNjnd2CB67e870=";
+
+          patches = [ rcu_patch ];
+       };
+    };
+
+    services.xserver = {
+      enable = true;
+      wacom.enable = true;
+      videoDrivers = [ "nvidia" ];
+      desktopManager = {
+        xterm.enable = false;
+        xfce.enable = true;
+      };
+      displayManager.defaultSession = "xfce";
+    };
+
+    environment.systemPackages = with pkgs; [
+      xfce.xfce4-whiskermenu-plugin
+    ];
 
     # firmware
     hardware.enableRedistributableFirmware = true;
