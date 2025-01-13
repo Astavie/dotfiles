14301357d7259899fb3ce91a82bba22245be579d
Astavie <astavie@pm.me>
2025-01-13 10:52:50 +0100

    add zen cache to persist

--- home/zen.nix
+++ home/zen.nix
@@ -7,6 +7,7 @@ in
   home.packages = [zen];
   asta.backup.directories = [
     "zen/.zen"
+    "zen/.cache/zen"
   ];
   home.file.".mime.types".text = ''
     type=text/plain exts=md,mkd,mkdn,mdwn,mdown,markdown, desc="Markdown document"
